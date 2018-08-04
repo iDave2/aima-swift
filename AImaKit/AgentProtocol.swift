201d7b81c3fe7fb4ea418376de7adf0b08f83a1b
iDave2 <dave.king@pobox.com>
2018-08-03 20:59:32 -0600

    Futzing with Xcode beta.

---
+++ AImaKit/AgentProtocol.swift
@@ -0,0 +1,28 @@
+//
+//  AgentProtocol.swift
+//  AImaKit
+//
+//  Created by Dave King on 8/2/18.
+//
+
+import Foundation
+
+/**
+ * 'An **agent** is anything that can be viewed as perceiving its
+ * __environment__ through __sensors__ and acting upon that environment
+ * through __actuators__.' -- AIMA3e, page 34.
+ *
+ * See [here](https://github.com/realm/jazzy/issues/992) for jazzy issue with
+ * quotes.
+ */
+public protocol Agent {
+    associatedtype ActionType
+    associatedtype PerceptType
+    func execute(_ percept: PerceptType) -> ActionType
+}
+
+public protocol JudgeProtocol {
+    associatedtype PerceptType
+    func execute(_ percept: PerceptType) -> Double
+}
+
